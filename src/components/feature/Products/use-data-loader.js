6ee0a6b8db46a0f2dd884c9eb9d9e377d2c3e41f
Simon Jefford <si@ibrokethat.com>
2020-05-23 12:26:27 +0100

    Renames files

--- src/components/feature/Products/use-data-loader.js
+++ src/components/feature/Products/use-data-loader.js
@@ -1,4 +1,4 @@
-import { dataLoaderFactory } from '../../../hooks/dataLoaderFactory';
+import { dataLoaderFactory } from '../../../hooks/data-loader-factory';
 import { loadProducts } from "../../../store/products/actions";
 
 export const useDataLoader = dataLoaderFactory(loadProducts, state => state.products.list)
