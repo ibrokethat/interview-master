fa4b33503ee479ccca596c871f2feb61564b387f
Simon Jefford <si@ibrokethat.com>
2020-05-24 11:20:55 +0100

    Adds tests for hooks

--- src/hooks/data-loader-factory.js
+++ src/hooks/data-loader-factory.js
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 export function dataLoaderFactory(action, selector) {
-  return () => {
+  return (...args) => {
     const dispatch = useDispatch();
     const items = useSelector(selector);
     useEffect(() => {
-      dispatch(action());
+      dispatch(action(...args));
     }, [dispatch]);
     return items || [];
   }
---
+++ src/hooks/data-loader-factory.test.js
@@ -0,0 +1,54 @@
+import {renderHook, act} from '../../test/utils';
+import { dataLoaderFactory } from './data-loader-factory';
+
+describe('hooks/data-loader-factory', () => {
+    let result;
+    let action;
+    let selector;
+    let useDataLoader;
+
+    describe('no data loaded', () => {
+      beforeAll(() => {
+        action = jest.fn().mockReturnValue({type: 'ACTION'});
+        selector = jest.fn().mockReturnValue(null);
+        useDataLoader = dataLoaderFactory(action, selector);
+
+        ({result} = renderHook(() => useDataLoader(1, 2)));
+      });
+
+      test('action is called', () => {
+          expect(action).toHaveBeenCalledWith(1, 2);
+      });
+
+      test('selector is called', () => {
+        expect(action).toHaveBeenCalled;
+      });
+
+      test('default empty array is returned', () => {
+          expect(result.current).toEqual([]);
+      });
+    });
+
+    describe('data is loaded', () => {
+      beforeAll(() => {
+        action = jest.fn().mockReturnValue({type: 'ACTION'});
+        selector = jest.fn().mockReturnValue([1, 2, 3, 4, 5]);
+        useDataLoader = dataLoaderFactory(action, selector);
+
+        ({result} = renderHook(() => useDataLoader()));
+      });
+
+      test('action is called', () => {
+        expect(action).toHaveBeenCalled;
+      });
+
+      test('selector is called', () => {
+        expect(action).toHaveBeenCalled;
+      });
+
+      test('data bound to selector is returned ', () => {
+        expect(result.current).toEqual([1, 2, 3, 4, 5]);
+      });
+    });
+
+});
